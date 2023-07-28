34e81b17289259e93861c33bade1f533ac92a60f
desislavdimitrov <desislavdimitrov@htmlbox.net>
2023-07-28 17:12:35 +0300

    Added homepage, card component, theme styles

---
+++ DocConnect/src/components/Card/Card.jsx
@@ -0,0 +1,51 @@
+import {Card as CardChakra, CardBody, Box, Image} from '@chakra-ui/react';
+
+const Card = ({Component, imageUrl, content}) => {
+  return (
+    <CardChakra
+      role='group'
+      variant='custom'
+    >
+      <Box
+        as='figure'
+        position='relative'
+        pb='89.63%'
+        overflow='hidden'
+        _before={
+          {
+            content: '""',
+            position: 'absolute',
+            zIndex: '1',
+            bottom: '0',
+            left: '0',
+            width: '100%',
+            height: '7rem',
+            bgGradient: 'linear(to-b, transparent, offwhite.400)',
+          }
+        }
+      >
+        <Image
+          src={imageUrl}
+          alt={content.specialtyNamed}
+          position='absolute'
+          inset='0'
+          w='100%'
+          h='100%'
+          objectFit='cover'
+          objectPosition='center center'
+          transition='transform .4s'
+          _groupHover={
+            {
+              transform: 'scale(1.05)',
+            }
+          }
+        />
+      </Box>
+      <CardBody>
+        <Component content={content} />
+      </CardBody>
+    </CardChakra>
+  );
+};
+
+export default Card;
---
+++ DocConnect/src/styles/theme/colors.theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  pink: {
+    50: '#fff2f8',
+  },
+  licorice: {
+    400: '#200017',
+  },
+  offwhite: {
+    400: '#fffffd',
+  },
+  bodyText: '#200017',
+};
+
+export default colors;
