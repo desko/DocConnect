0dcc3e9941a8a9d69fded3aa4a0db022753ccd13
Teodor Todorov <teodor.todorov@university.mentormate.com>
2023-07-31 16:01:01 +0300

    added specialties data fetch

--- DocConnect/src/store/features/specialtiesSlice.js
+++ DocConnect/src/store/features/specialtiesSlice.js
@@ -1,9 +1,16 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  isLoaded: false,
+  status: 'idle',
+  error: null,
   specialties: [],
 };
+
+export const fetchSpecialties = createAsyncThunk('specialties/fetchSpecialties', async () => {
+  const response = await axios.get(`ApiUrl/specialties`);
+  return response.json();
+});
 
 const specialtiesSlice = createSlice({
   name: 'specialties',
@@ -14,9 +21,22 @@ const specialtiesSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-
+    builder
+        .addCase(fetchSpecialties.pending, (state, action) =>{
+          state.status = 'loading';
+        })
+        .addCase(fetchSpecialties.fulfilled, (state, action) => {
+          state.status = 'succeeded';
+          state.specialties = action.payload;
+        })
+        .addCase(fetchSpecialties.rejected, (state, action) => {
+          state.status = 'failed';
+          state.error = action.error.message;
+        });
   },
 });
+
+export const selectAllSpecialties = (state) => state.specialties;
 
 // export const {} = specialtiesSlice.actions;
 export const specialtiesReducer = specialtiesSlice.reducer;
