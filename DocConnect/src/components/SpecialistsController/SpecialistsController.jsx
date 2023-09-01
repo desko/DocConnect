fb93c8bd42a2c5f1306210c4d2e73fee1b31b941
desislavdimitrov <desislavdimitrov@htmlbox.net>
2023-09-01 17:19:20 +0300

    Added loader specialist page

--- DocConnect/src/components/SpecialistsController/SpecialistsController.jsx
+++ DocConnect/src/components/SpecialistsController/SpecialistsController.jsx
@@ -1,4 +1,4 @@
-import {Box, Grid, GridItem} from '@chakra-ui/react';
+import {Box, Grid, GridItem, Spinner} from '@chakra-ui/react';
 import SearchSpecialists from '../SearchSpecialists/SearchSpecialists';
 import {useEffect, useCallback, useState, useRef} from 'react';
 import Card from '../Card/Card';
@@ -11,7 +11,7 @@ import {useDispatch, useSelector} from 'react-redux';
 const SpecialistsController = () => {
   const dispatch = useDispatch();
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const [loading, setLoading] = useState(false);
   const [specialists, setSpecialists] = useState([]);
   const [cityValue, setCityValue] = useState('');
   const [city, setCity] = useState(null);
@@ -42,9 +42,11 @@ const SpecialistsController = () => {
       );
 
       setSpecialists(result);
+      setLoading(false);
     };
 
     if (changed) {
+      setLoading(true);
       debounce(fetchData);
     }
   }, [city, name, specialty, setSpecialists, nameValue, changed, debounce]);
@@ -87,37 +89,44 @@ const SpecialistsController = () => {
         setChanged={setChanged}
       />
 
-      <Grid
-        isolation='isolate'
-        templateColumns={
-          {
-            base: '1fr',
-            sm: 'repeat(2, minmax(0, 1fr))',
-            lg: 'repeat(3, minmax(0, 1fr))',
+      {
+        loading &&
+        <Spinner color='red.400' size='xl' display='block' mt='3.6rem' mx='auto' />
+      }
+
+      {
+        !loading && <Grid
+          isolation='isolate'
+          templateColumns={
+            {
+              base: '1fr',
+              sm: 'repeat(2, minmax(0, 1fr))',
+              lg: 'repeat(3, minmax(0, 1fr))',
+            }
           }
-        }
-        gap={
-          {
-            base: '2rem',
-            md: '2rem 3rem',
-            lg: '5rem 6.2rem',
+          gap={
+            {
+              base: '2rem',
+              md: '2rem 3rem',
+              lg: '5rem 6.2rem',
+            }
           }
-        }
-        pt='3.6rem'
-      >
-        {
-          specialists.map((specialist, index) => {
-            return <GridItem key={specialist.id}>
-              <Card
-                Component={CardContentSpecialist}
-                imageUrl={specialist.imageUrl}
-                imagePrefix='https://cdntestredhf30zf.azureedge.net/'
-                content={specialist}
-              />
-            </GridItem>;
-          })
-        }
-      </Grid>
+          pt='3.6rem'
+        >
+          {
+            specialists.map((specialist, index) => {
+              return <GridItem key={specialist.id}>
+                <Card
+                  Component={CardContentSpecialist}
+                  imageUrl={specialist.imageUrl}
+                  imagePrefix='https://cdntestredhf30zf.azureedge.net/'
+                  content={specialist}
+                />
+              </GridItem>;
+            })
+          }
+        </Grid>
+      }
     </Box>
   );
 };
